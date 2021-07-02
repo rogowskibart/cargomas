c52c3e69170c2d32287606ed9f5793be8919d096
Bartosz Rogowski <rogowskibart@gmail.com>
2021-07-02 15:12:58 +0200

    Create TypKomponentu.java

---
+++ src/main/java/com/example/cargomas/domain/TypKomponentu.java
@@ -0,0 +1,13 @@
+package com.example.cargomas.domain;
+
+public enum TypKomponentu {
+    OPONA,
+    DETKA,
+    KOLO_PRZOD,
+    KOLO_TYL,
+    LAMPKA_PRZOD,
+    LAMPKA_TYL,
+    SIODELKO,
+    HAMULEC_PRZOD,
+    HAMULEC_TYL,
+}
