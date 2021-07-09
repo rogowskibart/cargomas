70a9ce9766f7c73779e23e2ab4d18b12544414aa
Bartosz Rogowski <rogowskibart@gmail.com>
2021-07-09 13:16:51 +0200

    Initial routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import BicycleList from './BicycleList';
+import Contact from './Contact';
+import Pricing from './Pricing';
+import StationList from './StationList';
 
 const App = () => {
-    return <div>App</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/bicycles" exact component={BicycleList} />
+          <Route path="/pricing" exact component={Pricing} />
+          <Route path="/contact" exact component={Contact} />
+          <Route path="/stations" exact component={StationList} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
